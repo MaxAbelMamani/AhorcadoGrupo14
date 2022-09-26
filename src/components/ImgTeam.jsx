4b12ae10c1c0eb7f686380c541247e30c0faa078
Agmor03 <112333132+Agmor03@users.noreply.github.com>
2022-09-26 18:41:43 -0300

    Agregado imagenes, Team.css e ImgTeam.jsx

---
+++ src/components/ImgTeam.jsx
@@ -0,0 +1,13 @@
+import ImgDesMAM from '../assets/images/MaxiMamani.jpg'
+import ImgDesMGO from '../assets/images/Matias_Ortega.jpg'
+import ImgDesMLB from '../assets/images/MaxiBustamante.jpg'
+import ImgDesAMAM from '../assets/images/AgusMorales.jpg'
+import ImgDesMACA from '../assets/images/ImagenEjemplo.jpg'
+
+export default {
+    "MaxiMamani": ImgDesMAM,
+    "MatiOrtega": ImgDesMGO,
+    "MaxiBustamante": ImgDesMLB,
+    "AgusMorales": ImgDesAMAM,
+    "MatiCruz": ImgDesMACA
+}
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../assets/styles/Team.css'
+import '../assets/styles/Button.css'
+import images from '../components/ImgTeam.jsx'
 function Team() {
   return (
-    <div>
+    <div className="container-team">
       <h1>Equipo de Trabajo</h1>
-      <div>
+      <div className="team">
         <div className="NyF">
-          <img src="#" alt="..." />
+          <img src={images.MaxiMamani} alt="..." />
           <a
             className="perfil-github"
             target="_blank"
@@ -18,7 +21,7 @@ function Team() {
           </a>
         </div>
         <div className="NyF">
-          <img src="#" alt="..." />
+          <img src={images.MatiOrtega} alt="..." />
           <a
             className="perfil-github"
             target="_blank"
@@ -30,7 +33,7 @@ function Team() {
           </a>
         </div>
         <div className="NyF">
-          <img src="#" alt="..." />
+          <img src={images.MaxiBustamante} alt="..." />
           <a
             className="perfil-github"
             target="_blank"
@@ -42,7 +45,7 @@ function Team() {
           </a>
         </div>
         <div className="NyF">
-          <img src="#" alt="..." />
+          <img src={images.AgusMorales} alt="..." />
           <a
             className="perfil-github"
             target="_blank"
@@ -54,12 +57,12 @@ function Team() {
           </a>
         </div>
         <div className="NyF">
-          <img src="#" alt="..." />
+          <img src={images.MatiCruz} alt="..." />
           <a
             className="perfil-github"
             target="_blank"
             rel="noreferrer"
-            href="https://github.com/Agmor03"
+            href="https://github.com/MatiasCA11"
           >
             {" "}
             Cruz Aruzamen Matias A.{" "}
@@ -67,7 +70,7 @@ function Team() {
         </div>
       </div>
       <div className="boton-team">
-        <Link to={"/"}>Inicio</Link>
+        <Link to={"/"} className="btn">Inicio</Link>
       </div>
     </div>
   );
