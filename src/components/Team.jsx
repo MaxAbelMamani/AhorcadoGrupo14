988a3f48a66d734e62d2a17ffc8d0470f8aa3aed
MatiasCA11 <matiasca619@gmail.com>
2022-10-07 19:38:24 -0300

    comentarios, importe de imagen

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// Importe de los estilos .css
 import '../assets/styles/Team.css';
 import '../assets/styles/Button.css';
+// Importe de funciones
 import equipo from './teams.json';
 import Developers from './Developers';
 
@@ -14,6 +16,7 @@ function Team() {
           equipo.map((e) => {
             return (
               <Developers key={e.id} nombre={e.nombre} Imagen={e.src} Edad={e.Edad} Frase={e.Frase} github={e.github} />
+              //armado de elementos en cajas de "desarrolladores"
             )
           })
         }
